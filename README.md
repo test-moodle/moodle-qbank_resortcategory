2c756dd25ed66b821a1c66a5b165227a4e8c5387
Vadim Dvorovenko <Vadimon@mail.ru>
2016-04-16 15:06:19 +0700

    Initial release

---
+++ README.md
@@ -0,0 +1,30 @@
+Resort question categories Moodle plugin
+========================================
+
+Requirements
+------------
+- Moodle 3.0 (build 2015051100) or later.
+
+Installation
+------------
+Copy the resortquestioncategory folder into your Moodle /local directory and visit your Admin Notification page to complete the installation.
+
+Usage
+-----
+Question bank navigation node will be extended with "Sort categories" item. Select category you want to sort. All subcategories will
+be resorted in alphabetical order. This plugin may be useful to manage large question bank together with local_renumberquestioncategory 
+plugin.
+
+Author
+------
+- Vadim Dvorovenko (Vadimon@mail.ru)
+
+Links
+-----
+- Updates: https://moodle.org/plugins/view.php?plugin=local_resortquestioncategory
+- Latest code: https://github.com/vadimonus/moodle-local_resortquestioncategory
+
+Changes
+-------
+Release 0.9 (build 2016041601):
+- Initial release.
---
+++ lang/en/local_resortquestioncategory.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tool for sorting question categories in alphabetical order.
+ *
+ * @package    local_resortquestioncategory
+ * @copyright  2016 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Resort question categories';
+$string['resortcategory'] = 'Sort categories';
+$string['resortthiscategory'] = 'Sort this category';
+$string['selectcategory'] = 'Select category to sort';
+$string['selectcategoryinfo'] = 'Specify category you want to resort. All subcategories will be resorted in alphabetical order. Select "Top" to sort all categories in context.';
