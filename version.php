0bf61fa5aa407ebe868f360965194182fe282dd3
Vadim Dvorovenko <Vadimon@mail.ru>
2020-06-13 11:37:03 +0700

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_resortquestioncategory';
-$plugin->version   = 2016051000;
-$plugin->release = '1.0';
+$plugin->version   = 2020061300;
+$plugin->release = '1.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2015111600; // Moodle 3.0.
